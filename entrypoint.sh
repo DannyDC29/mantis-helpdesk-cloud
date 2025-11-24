c800059d9068645e596506db5d61d13a0539f223
DannyDC29 <142054872+DannyDC29@users.noreply.github.com>
2025-11-23 21:28:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,18 @@
 
 cat <<EOF > /var/www/html/config/config_inc.php
 <?php
-\$g_hostname = '${DB_HOST}';
-\$g_db_type = '${DB_TYPE}';
-\$g_database_name = '${DB_NAME}';
-\$g_db_username = '${DB_USER}';
-\$g_db_password = '${DB_PASSWORD}';
-\$g_db_port = ${DB_PORT};
+    \$g_hostname = '${DB_HOST}';
+    \$g_db_type = '${DB_TYPE}';
+    \$g_database_name = '${DB_NAME}';
+    \$g_db_username = '${DB_USER}';
+    \$g_db_password = '${DB_PASSWORD}';
+    \$g_db_port = ${DB_PORT};
 
-\$g_default_timezone = 'America/Bogota';
-\$g_crypto_master_salt = '$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 30)';
-\$g_admin_checks = OFF;
+    \$g_default_timezone = 'America/Bogota';
+    \$g_crypto_master_salt = '$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 30)';
+
+    \$g_allow_signup = ON;
+    \$g_admin_checks = OFF;
 ?>
 EOF
 
