6fac236e3b11bfd11ed22813427c058bca48447a
DannyDC29 <daniela.diazc@cecar.edu.co>
2025-11-24 01:24:56 -0500

    vamo a ver again x8

--- config/config_inc.php
+++ config/config_inc.php
@@ -1,11 +1,11 @@
 <?php
 
 $g_db_type = 'mysqli';
-$g_hostname = 'yamabiko.proxy.rlwy.net';
+$g_hostname = 'mysql.railway.internal';
 $g_db_username = 'root';
 $g_db_password = 'RvyyjdRVXfXpaiPoKtnqzznNKDWFJnNZ';
 $g_database_name = 'railway';
-$g_db_port = 43656;
+$g_db_port = 3306;
 
 $g_crypto_master_salt = 'Xr93sA7gKM1tPQ2wEfL9cUz4bJs0DyNh';
 $g_default_timezone = 'UTC';
