b99e7c2135196aa020c0d4d8d31ecffa1a511724
DannyDC29 <daniela.diazc@cecar.edu.co>
2025-11-23 20:16:01 -0500

    Add final Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 FROM vimagick/mantisbt:latest
 
-# Variables de entorno (Railway las inyecta)
 ENV DB_TYPE=mysqli
-ENV DB_HOST=${MYSQLHOST}
-ENV DB_NAME=${MYSQLDATABASE}
-ENV DB_USER=${MYSQLUSER}
-ENV DB_PASSWORD=${MYSQLPASSWORD}
-
-# Crear config_inc.php automáticamente
-RUN mkdir -p /var/www/html/config
+ENV DB_HOST=yamabiko.proxy.rlwy.net
+ENV DB_PORT=43656
+ENV DB_NAME=railway
+ENV DB_USER=root
+ENV DB_PASSWORD=RvyyjdRVXfXpaiPoKtnqzznNKDWFJnNZ
 
 COPY ./entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 EXPOSE 80
-
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Crear archivo config_inc.php basado en variables de entorno
 cat <<EOF > /var/www/html/config/config_inc.php
 <?php
 \$g_hostname = '${DB_HOST}';
@@ -8,15 +7,12 @@ cat <<EOF > /var/www/html/config/config_inc.php
 \$g_database_name = '${DB_NAME}';
 \$g_db_username = '${DB_USER}';
 \$g_db_password = '${DB_PASSWORD}';
+\$g_db_port = ${DB_PORT};
 
-# Opciones necesarias para producción en Railway
-\$g_default_timezone       = 'America/Bogota';
-\$g_crypto_master_salt     = '$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)';
-
-# Crear usuario administrador por defecto
+\$g_default_timezone = 'America/Bogota';
+\$g_crypto_master_salt = '$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 30)';
 \$g_admin_checks = OFF;
 ?>
 EOF
 
-# Arrancar Apache
 apache2-foreground
