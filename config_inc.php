674e3e4a2c1be0c2bcc57a7354ed0b52fd95e1b6
DannyDC29 <daniela.diazc@cecar.edu.co>
2025-11-24 01:04:18 -0500

    vamo a ver again x4

--- config_inc.php
+++ config_inc.php
@@ -1,18 +1,11 @@
 <?php
 
-$g_hostname = 'yamabiko.proxy.rlwy.net';
 $g_db_type = 'mysqli';
-$g_database_name = 'railway';
+$g_hostname = 'yamabiko.proxy.rlwy.net';
 $g_db_username = 'root';
 $g_db_password = 'RvyyjdRVXfXpaiPoKtnqzznNKDWFJnNZ';
+$g_database_name = 'railway';
 $g_db_port = 43656;
 
+$g_crypto_master_salt = 'Xr93sA7gKM1tPQ2wEfL9cUz4bJs0DyNh';
 $g_default_timezone = 'UTC';
-
-/**
- * crypto_master_salt
- * MUST be at least 16 characters, ONLY ASCII letters/numbers allowed.
- */
-$g_crypto_master_salt = 'Km49Fh28Ds91Lp73QcXw9Ze81RtP6Uy3';
-
-?>
