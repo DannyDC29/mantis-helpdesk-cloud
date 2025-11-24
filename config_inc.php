a46c9aaaeb97f101111b47d14cf4c7df72e8e498
DannyDC29 <142054872+DannyDC29@users.noreply.github.com>
2025-11-23 23:20:59 -0500

    Update config_inc.php

--- config_inc.php
+++ config_inc.php
@@ -1,10 +1,10 @@
 <?php
-$g_hostname = getenv('MYSQLHOST');
-$g_db_type = 'mysqli';
-$g_database_name = getenv('MYSQLDATABASE');
-$g_db_username = getenv('MYSQLUSER');
-$g_db_password = getenv('MYSQLPASSWORD');
-$g_db_port = getenv('MYSQLPORT');
+$g_hostname      = 'yamabiko.proxy.rlwy.net';
+$g_db_username   = 'root';
+$g_db_password   = 'RvyyjdRVXfXpaiPoKtnqzznNKDWFJnNZ';
+$g_database_name = 'railway';
+$g_db_type       = 'mysqli';
+$g_db_port       = 43656;
 
 $g_default_timezone = 'America/Bogota';
 $g_crypto_master_salt = 'xP3sVCKmNA4jRpXyT4F78G6LqKD3fS';
