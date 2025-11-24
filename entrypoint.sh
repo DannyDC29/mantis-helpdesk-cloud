c0e0eba5f295e4bc108ab7008254effab841ce78
DannyDC29 <142054872+DannyDC29@users.noreply.github.com>
2025-11-23 21:41:29 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,14 @@
 
 cat <<EOF > /var/www/html/config/config_inc.php
 <?php
-\$g_hostname = '${DB_HOST}';
-\$g_db_type = '${DB_TYPE}';
-\$g_database_name = '${DB_NAME}';
-\$g_db_username = '${DB_USER}';
-\$g_db_password = '${DB_PASSWORD}';
-\$g_db_port = ${DB_PORT};
-
+\$g_hostname = getenv('DB_HOST');
+\$g_db_type = 'mysqli';
+\$g_database_name = getenv('DB_NAME');
+\$g_db_username = getenv('DB_USER');
+\$g_db_password = getenv('DB_PASSWORD');
+\$g_db_port = getenv('DB_PORT');
 \$g_default_timezone = 'America/Bogota';
-\$g_crypto_master_salt = '$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 30)';
-\$g_admin_checks = OFF;
 ?>
 EOF
 
-apache2-foreground
+exec "$@"
