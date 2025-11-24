b5c38c224913b38d4d2060e05e17eb5777d4fa7b
DannyDC29 <142054872+DannyDC29@users.noreply.github.com>
2025-11-23 23:03:08 -0500

    Create config_inc.php

---
+++ config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname = getenv('MYSQLHOST');
+$g_db_type = 'mysqli';
+$g_database_name = getenv('MYSQLDATABASE');
+$g_db_username = getenv('MYSQLUSER');
+$g_db_password = getenv('MYSQLPASSWORD');
+$g_db_port = getenv('MYSQLPORT');
+
+$g_default_timezone = 'America/Bogota';
+$g_crypto_master_salt = 'xP3sVCKmNA4jRpXyT4F78G6LqKD3fS';
+$g_admin_checks = OFF;
+?>
